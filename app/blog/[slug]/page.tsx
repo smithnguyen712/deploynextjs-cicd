96eb647a7cbdf731d5dbabb3831375f9616e05cd
smithnguyen712 <tungnt71289@gmail.com>
2026-07-07 15:00:48 +0700

    fixed

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,6 +4,10 @@ type Params = {
   };
 };
 
+export async function generateStaticParams() {
+  return [{ slug: "Test" }];
+}
+
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
