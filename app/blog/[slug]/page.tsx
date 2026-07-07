5a55cf7e93b074b3fa898279d76634d1a0be1c9c
smithnguyen712 <tungnt71289@gmail.com>
2026-07-07 14:19:36 +0700

    update code

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,5 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>HoleTex</p></>
+  return <><h1>Slug: {params.slug}</h1><p>HoleTex</p><p>WelcomeLabSmith</p></>
 }
